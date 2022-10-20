9025fafe5e93af18facd998e5fdfc87f7c731f20
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-20 12:07:03 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,26 @@ type Props = {}
 
 export default function Header({}: Props) {
   return  (
-    <header>
-    <div>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto'>
+    <div className='flex flex-row items-center'>
       {/* social icons */}
-      <SocialIcon url="https://twitter.com/jaketrent" fgColor='gray' bgColor='transparent' />
+      <SocialIcon url="https://twitter.com/jaketrent" 
+      fgColor='gray' bgColor='transparent' />
+
+      <SocialIcon url="https://twitter.com/jaketrent" 
+      fgColor='gray' bgColor='transparent' />
+
+      <SocialIcon url="https://twitter.com/jaketrent"
+      fgColor='gray' bgColor='transparent' />
     </div>
+
+    {/* contact me */}
+    <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+    <SocialIcon className='cursor-pointer' network='email'
+      fgColor='gray' bgColor='transparent' />
+      <p className='hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+    </div>
+    
     </header>
   );
 }
