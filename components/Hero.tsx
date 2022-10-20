9a8a6e89f679a6c2c2e6fd955266f83ec09e87f8
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-20 18:12:24 +0200

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,9 +22,9 @@ export default function Hero({}: Props) {
         <img className='relativ rounded-full h-32 w-32 mx-auto object-cover' src="https://images.unsplash.com/flagged/photo-1577614742608-393919945ee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="foto" />
         
         <div>
-          <h2 className='text-sm text-gray-500'>Fullstack Developer</h2>
-          <h1>
-          <span>{text}</span>
+          <h2 className='text-sm text-gray-500 pb-2 tracking-[15px]'>Fullstack Developer</h2>
+          <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'>
+          <span className='mr-3'>{text}</span>
           <Cursor cursorColor='#F7AB0A'/>
           </h1>
         </div>
