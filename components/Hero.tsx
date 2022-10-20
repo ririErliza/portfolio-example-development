afd411b47a148261616cad2520b62ed03086c867
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-20 17:48:47 +0200

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
+
 import React from 'react'
 import {Cursor, useTypewriter} from 'react-simple-typewriter'
 import BackgroundCircle from './BackgroundCircle'
+
 
 type Props = {}
 
@@ -15,12 +17,18 @@ export default function Hero({}: Props) {
     delaySpeed: 2000,
     })
   return (
-    <div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircle/>
-        <h1>
-        <span>{text}</span>
-        <Cursor cursorColor='#F7AB0A'/>
-        </h1>
+        <img className='relativ rounded-full h-32 w-32 mx-auto object-cover' src="https://images.unsplash.com/flagged/photo-1577614742608-393919945ee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="foto" />
+        
+        <div>
+          <h2 className='text-sm text-gray-500'>Fullstack Developer</h2>
+          <h1>
+          <span>{text}</span>
+          <Cursor cursorColor='#F7AB0A'/>
+          </h1>
+        </div>
+        
         
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 '>
       <Head>
         <title >Portfolio Example using NextJS and TailwindCSS</title>
         <meta name="description" content="portfolio example developed using next js, tailwind css" />
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
     <Header />
 
     {/* Hero */}
-    <section id="hero">
+    <section id="hero" className='snap-center'>
     <Hero/>
     </section>
 
