635096b3d4c5e6d591f651c627d2874bd7e8dba6
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-21 15:56:32 +0200

    snap

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ export default function Hero({}: Props) {
     delaySpeed: 2000,
     })
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center'>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
       <BackgroundCircle/>
         {/*
         <img className='relativ rounded-full h-32 w-32 mx-auto object-cover' src="https://images.unsplash.com/flagged/photo-1577614742608-393919945ee2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" alt="foto" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll'>
       <Head>
         <title >Portfolio Example using NextJS and TailwindCSS</title>
         <meta name="description" content="portfolio example developed using next js, tailwind css" />
