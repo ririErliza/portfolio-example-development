3427e8a736e0da073a58cf5aeb57f60fde2e65f9
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-21 16:00:40 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title >Portfolio Example using NextJS and TailwindCSS</title>
         <meta name="description" content="portfolio example developed using next js, tailwind css" />
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <Header />
 
-      <section id="hero" className=''>
+      <section id="hero" className='snap-center'>
         <Hero />
       </section>
 
