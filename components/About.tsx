b1bca83e1460cb46974b74378f2098a3cf58d07a
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-24 11:35:44 +0200

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -23,7 +23,15 @@ export default function About({}: Props) {
         }}
         viewport={{once:true}}
         src="https://img.okezone.com/okz/500/library/images/2019/05/07/p7olvqf6t28kw4may1xw_13671.jpg"
-        alt="" />
+        alt="foto"
+        className='mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] object-cover' />
+
+        <div className='px-0 md:px-10'>
+          <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#F7AB0A]/50'>little</span> background</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio corrupti quo neque praesentium explicabo consequatur sit sunt! Quam natus iste quasi perspiciatis enim, veritatis qui laboriosam, officiis magni quisquam earum
+          </p>
+        </div>
         
     </div>
   )
